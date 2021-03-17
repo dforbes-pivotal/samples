5e154a5ade76f36dd50d5adc58270fe966d1fd08
Donald Forbes <dforbes@donaldf-a01.vmware.com>
2021-03-17 16:16:18 +0000

    Changing colour.

--- go/no-imports/main.go
+++ go/no-imports/main.go
@@ -15,7 +15,7 @@ const INDEX = `<!DOCTYPE html>
   <body>
     <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
     <br><br>
-    <h><b>Example go application, as built and managed by <p style="color:Blue;">Tanzu Build Service</p>.</b></h><br>
+    <h><b>Example go application, as built and managed by <span style="color:Red;">Tanzu Build Service</span>.</b></h><br>
     <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://docs.vmware.com/en/VMware-Tanzu-Kubernetes-Grid/images/GUID-8546DDD9-718A-42F7-9EDB-0BCC3A316BB6-low.png"></img>
   </body>
 </html>`
